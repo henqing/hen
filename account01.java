e3287f0bb52414650fec9c1b2450b48baff499ae
henqing <1922217612@qq.com>
2019-09-15 23:02:32 +0800

    初次提交

---
+++ account01.java
@@ -0,0 +1,9 @@
+public class account01{
+	public static void main(String[] args){
+		int a=9;
+		System.out.println(a+=10);
+		System.out.println(a-=5);
+		System.out.println(a*=8);
+		System.out.println(a/=3);
+	}
+}
